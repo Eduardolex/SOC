653113ab2840ffcf60bea739952ee91543c55f77
Eduardolex <ntomer010@gmail.com>
2026-02-03 09:42:56 -0800

    fiji3gjeirji

--- SOC.py
+++ SOC.py
@@ -3,25 +3,40 @@ import random
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import deque
 
-
-
-
-L = 5
+L = 64
+THRESH = 4
 grid = np.zeros((L, L), dtype=int)
+rng = random.Random()
 
-for step in range(20):
-    i = random.randrange(L)
-    j = random.randrange(L)
-    
+def drive():
+    i = rng.randrange(L)
+    j = rng.randrange(L)
     grid[i, j] += 1
-    if grid[i, j] >= 4:
-     grid[i, j] = 0
-     
+    return (i, j)
 
-    print(f"step {step}, max height {grid.max()}")
+def relax():
+    q = deque(np.argwhere(grid >= THRESH))
+    topplings = 0
 
-    plt.imshow(grid)
+    while q:
+        x, y = q.popleft()
+        if grid[x, y] < THRESH:
+            continue
+
+        grid[x, y] -= THRESH
+        topplings += 1
+
+        for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < L and 0 <= ny < L:
+                grid[nx, ny] += 1
+                if grid[nx, ny] >= THRESH:
+                    q.append((nx, ny))
+    return topplings            
+print(grid)
+plt.imshow(grid)
 plt.colorbar()
 plt.show()
 # Simple implementation of a 2D Sandpile Model (SOC)
